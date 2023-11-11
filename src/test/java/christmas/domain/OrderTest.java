912b3aa0a08cb6b4a662e86c9d8bb0236326c310
Zeus6768 <akaps6768@gmail.com>
2023-11-12 01:56:34 +0900

    test: 할인 전 총주문 금액 생성

---
+++ src/test/java/christmas/domain/OrderTest.java
@@ -0,0 +1,33 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import christmas.domain.eventplanner.Order;
+import christmas.domain.menu.Menus;
+import util.MyChristmasTest;
+
+class OrderTest extends MyChristmasTest {
+
+    @Test
+    void totalPriceTest() {
+        // given
+        Order order = Order.from(
+                Map.of(
+                        Menus.find("티본스테이크"), 1,
+                        Menus.find("바비큐립"), 1,
+                        Menus.find("초코케이크"), 2,
+                        Menus.find("제로콜라"), 1
+                )
+        );
+
+        // when
+        int actual = order.totalPrice();
+
+        // then
+        assertThat(actual).isEqualTo(142_000);
+    }
+}
