3c0c13f9cd7fc0c1d4e13b909fa1eeb70467acb0
Zeus6768 <akaps6768@gmail.com>
2023-11-11 22:19:17 +0900

    feat: 입력 예외 처리기능 구현

---
+++ src/main/java/christmas/view/exception/InputExceptionHandler.java
@@ -0,0 +1,19 @@
+package christmas.view.exception;
+
+import java.util.NoSuchElementException;
+import java.util.function.Supplier;
+
+public class InputExceptionHandler {
+
+    private static final String ERROR = "[ERROR] ";
+
+    public <T> T handle(Supplier<T> callback, final String message) {
+        while (true) {
+            try {
+                return callback.get();
+            } catch (IllegalArgumentException | NoSuchElementException | ArrayIndexOutOfBoundsException exception) {
+                System.out.println(ERROR + message);
+            }
+        }
+    }
+}
