b84e59ec9ca6fbb1ccd266aa9a9df24d830642ac
Zeus6768 <akaps6768@gmail.com>
2023-11-14 18:56:49 +0900

    refactor: 이벤트 혜택 메서드 분리

--- src/main/java/christmas/domain/eventbenefit/Event.java
+++ src/main/java/christmas/domain/eventbenefit/Event.java
@@ -1,10 +1,10 @@
 package christmas.domain.eventbenefit;
 
 import static christmas.domain.eventbenefit.EventBenefit.NO_BENEFIT;
-import static christmas.domain.eventplanner.EventCalendar.isXMasDDay;
 import static christmas.domain.eventplanner.EventCalendar.isSpecialDay;
 import static christmas.domain.eventplanner.EventCalendar.isWeekEnd;
 import static christmas.domain.eventplanner.EventCalendar.isWeekday;
+import static christmas.domain.eventplanner.EventCalendar.isXMasDDay;
 import static christmas.domain.eventplanner.EventConstant.GIFT_EVENT_TOTAL_MIN;
 import static christmas.domain.eventplanner.EventConstant.SPECIAL_EVENT_TOTAL_DISCOUNT;
 import static christmas.domain.eventplanner.EventConstant.WEEKDAY_EVENT_DESSERT_DISCOUNT;
@@ -25,57 +25,15 @@ import christmas.domain.menu.Menu;
 
 public enum Event {
 
-    X_MAS_D_DAY("크리스마스 디데이 할인",
-            (visitDate, order) -> {
-                if (isXMasDDay(visitDate)) {
-                    return EventDiscountBenefit.from(
-                            visitDate.getDay() * X_MAS_EVENT_DAILY_DISCOUNT + X_MAS_EVENT_DEFAULT_DISCOUNT
-                    );
-                }
-                return NO_BENEFIT;
-            }),
+    X_MAS_D_DAY("크리스마스 디데이 할인", ((visitDate, order) -> getXMasEventBenefit(visitDate))),
 
-    WEEKDAY("평일 할인",
-            (visitDate, order) -> {
-                if (isWeekday(visitDate)) {
-                    return EventDiscountBenefit.from(
-                            benefitFromOrder(
-                                    order,
-                                    menu -> menu instanceof Dessert,
-                                    WEEKDAY_EVENT_DESSERT_DISCOUNT
-                            ));
-                }
-                return NO_BENEFIT;
-            }),
+    WEEKDAY("평일 할인", Event::getWeekdayEventBenefit),
 
-    WEEKEND("주말 할인",
-            (visitDate, order) -> {
-                if (isWeekEnd(visitDate)) {
-                    return EventDiscountBenefit.from(
-                            benefitFromOrder(
-                                    order,
-                                    menu -> menu instanceof MainDish,
-                                    WEEKEND_EVENT_MAIN_DISH_DISCOUNT
-                            ));
-                }
-                return NO_BENEFIT;
-            }),
+    WEEKEND("주말 할인", Event::getWeekendEventBenefit),
 
-    SPECIAL("스페셜 할인",
-            (visitDate, order) -> {
-                if (isSpecialDay(visitDate)) {
-                    return EventDiscountBenefit.from(SPECIAL_EVENT_TOTAL_DISCOUNT);
-                }
-                return NO_BENEFIT;
-            }),
+    SPECIAL("스페셜 할인", Event::getSpecialEventBenefit),
 
-    GIFT("증정 이벤트",
-            (visitDate, order) -> {
-                if (order.totalPrice() >= GIFT_EVENT_TOTAL_MIN) {
-                    return EventGiftBenefit.from(CHAMPAGNE);
-                }
-                return EventGiftBenefit.from(NO_MENU);
-            });
+    GIFT("증정 이벤트", ((visitDate, order) -> getGiftEventBenefit(order)));
 
     private final String name;
     private final BiFunction<VisitDate, Order, EventBenefit> benefit;
@@ -85,18 +43,63 @@ public enum Event {
         this.benefit = benefit;
     }
 
-    private static int benefitFromOrder(Order order, Predicate<Menu> condition, int discount) {
-        return order.stream()
-                .filter(menuAndCount -> condition.test(menuAndCount.getKey()))
-                .mapToInt(menuAndCount -> menuAndCount.getValue() * discount)
-                .sum();
-    }
-
     public String getName() {
         return name;
     }
 
     public EventBenefit getBenefitOf(VisitDate visitDate, Order order) {
         return benefit.apply(visitDate, order);
+    }
+
+    private static EventBenefit getXMasEventBenefit(VisitDate visitDate) {
+        if (isXMasDDay(visitDate)) {
+            return EventDiscountBenefit.from(
+                    visitDate.getDay() * X_MAS_EVENT_DAILY_DISCOUNT + X_MAS_EVENT_DEFAULT_DISCOUNT
+            );
+        }
+        return NO_BENEFIT;
+    }
+
+    private static EventBenefit getWeekdayEventBenefit(VisitDate visitDate, Order order) {
+        if (isWeekday(visitDate)) {
+            int totalPrice = getBenefitPriceFromOrder(
+                    order,
+                    menu -> menu instanceof Dessert,
+                    WEEKDAY_EVENT_DESSERT_DISCOUNT);
+            return EventDiscountBenefit.from(totalPrice);
+        }
+        return NO_BENEFIT;
+    }
+
+    private static EventBenefit getWeekendEventBenefit(VisitDate visitDate, Order order) {
+        if (isWeekEnd(visitDate)) {
+            int totalPrice = getBenefitPriceFromOrder(
+                    order,
+                    menu -> menu instanceof MainDish,
+                    WEEKEND_EVENT_MAIN_DISH_DISCOUNT);
+            return EventDiscountBenefit.from(totalPrice);
+        }
+        return NO_BENEFIT;
+    }
+
+    private static EventBenefit getSpecialEventBenefit(VisitDate visitDate, Order order) {
+        if (isSpecialDay(visitDate)) {
+            return EventDiscountBenefit.from(SPECIAL_EVENT_TOTAL_DISCOUNT);
+        }
+        return NO_BENEFIT;
+    }
+
+    private static EventBenefit getGiftEventBenefit(Order order) {
+        if (order.totalPrice() >= GIFT_EVENT_TOTAL_MIN) {
+            return EventGiftBenefit.from(CHAMPAGNE);
+        }
+        return EventGiftBenefit.from(NO_MENU);
+    }
+
+    private static int getBenefitPriceFromOrder(Order order, Predicate<Menu> condition, int discount) {
+        return order.stream()
+                .filter(menuAndCount -> condition.test(menuAndCount.getKey()))
+                .mapToInt(menuAndCount -> menuAndCount.getValue() * discount)
+                .sum();
     }
 }
--- src/main/java/christmas/domain/eventbenefit/EventBadge.java
+++ src/main/java/christmas/domain/eventbenefit/EventBadge.java
@@ -1,32 +1,35 @@
 package christmas.domain.eventbenefit;
 
 import java.util.Arrays;
-import java.util.function.Function;
 
 import christmas.domain.eventplanner.EventPlanResult;
 
 public enum EventBadge {
-    SANTA("산타", (totalBenefit) -> totalBenefit >= 20_000),
-    TREE("트리", (totalBenefit) -> totalBenefit >= 10_000),
-    STAR("스타", (totalBenefit) -> totalBenefit >= 5_000),
-    NO_BADGE("", (totalBenefit) -> true);
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("스타", 5_000),
+    NO_BADGE("", 0);
 
     private final String name;
-    private final Function<Integer, Boolean> condition;
+    private final int minTotalBenefit;
 
-    EventBadge(String name, Function<Integer, Boolean> condition) {
+    EventBadge(String name, int minTotalBenefit) {
         this.name = name;
-        this.condition = condition;
+        this.minTotalBenefit = minTotalBenefit;
     }
 
     public static EventBadge from(EventPlanResult result) {
         return Arrays.stream(values())
-                .filter(badge -> badge.condition.apply(result.getTotalBenefitAmount()))
+                .filter(badge -> badge.isQualified(result.getTotalBenefitAmount()))
                 .findFirst()
                 .orElse(NO_BADGE);
     }
 
     public String getName() {
         return name;
+    }
+
+    private boolean isQualified(int totalBenefit) {
+        return minTotalBenefit <= totalBenefit;
     }
 }
