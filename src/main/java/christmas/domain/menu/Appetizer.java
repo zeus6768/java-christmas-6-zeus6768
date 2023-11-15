291f27297bcc873766e20039183b05531362c6aa
Zeus6768 <akaps6768@gmail.com>
2023-11-15 22:33:45 +0900

    fix: 오타 수정

--- src/main/java/christmas/domain/menu/Appetizer.java
+++ src/main/java/christmas/domain/menu/Appetizer.java
@@ -2,7 +2,7 @@ package christmas.domain.menu;
 
 public enum Appetizer implements Menu {
 
-    WHITE_MUSHROOM_SOUP("양송이스프", 6_000),
+    WHITE_MUSHROOM_SOUP("양송이수프", 6_000),
     TAPAS("타파스", 5_500),
     CAESAR_SALAD("시저샐러드", 8_000);
 
